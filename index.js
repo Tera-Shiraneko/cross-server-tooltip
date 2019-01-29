6704750f63f5834f6214e482c58ab7cd8d4ed937
Shalltearz <42479034+Tera-Shiraneko@users.noreply.github.com>
2019-01-29 23:33:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const SettingsUI = require('tera-mod-ui').Settings;
 
 module.exports = function Crossservertooltip(mod) {
 
-    if (mod.proxyAuthor !== 'caali' || !global.TeraProxy)
-        mod.warn('You are trying to use this module on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future.');
+    if (mod.proxyAuthor !== 'caali' || !global.TeraProxy) {
+        mod.warn('You are trying to use this module on an unsupported legacy version of tera-proxy.');
+        mod.warn('The module may not work as expected, and even if it works for now, it may break at any point in the future!');
+        mod.warn('It is highly recommended that you download the latest official version from the #proxy channel in http://tiny.cc/caalis-tera-proxy');
+    }
 
     let item, paperdoll,
         crystals = [];
